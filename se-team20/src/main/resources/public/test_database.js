0e26f6664b094bcece40fc5e0ef85b8d1c20f0af
CrookedDesk <50561427+CrookedDesk@users.noreply.github.com>
2020-10-01 10:53:54 +1300

    testing .xhr

---
+++ se-team20/src/main/resources/public/test_database.js
@@ -0,0 +1,21 @@
+var xhr = createRequest();
+function connect(dataSource)
+{
+	if(xhr)
+	{
+		var obj = document.getElementById(divID);
+		var requestbody = "test="+encodeURIComponent("test");
+		
+		xhr.open("POST", dataSource, true);
+		xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+		xhr.onreadystatechange = function()
+		{
+			//alert(xhr.readyState);
+			if (xhr.readyState == 4 && xhr.status == 200)
+			{
+				obj.innerHTML = xhr.responseText;
+			}
+		}
+		xhr.send(requestbody);
+	}
+}
