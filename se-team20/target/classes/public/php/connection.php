55cfefce4fdca14b2e68a43bebe844167259b77d
CrookedDesk <50561427+CrookedDesk@users.noreply.github.com>
2020-10-01 10:32:10 +1300

    switching to heroku db

---
+++ se-team20/target/classes/public/php/connection.php
@@ -0,0 +1,8 @@
+<?php>
+$host = "ec2-23-20-168-40.compute-1.amazonaws.com"
+$user = "fgltmkgurkvrcy"
+$pswd = "db66c2b072423e5956ec608256cd4a64a56ac5d9c796065f5bcc08eaa90f38e4"
+$dbnm = "d97j5rjcvk44a7"
+
+$conn = @mysqli_connect($host,$user,$pswd,$dbnm) or die ("DB Connection Failed");
+?>
