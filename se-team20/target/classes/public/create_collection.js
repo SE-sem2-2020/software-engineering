42ddf4e48a9fcd80ce3bd72067719dfffc727eb8
CrookedDesk <50561427+CrookedDesk@users.noreply.github.com>
2020-10-01 10:18:12 +1300

    Testing MongoDB

--- se-team20/target/classes/public/create_collection.js
+++ se-team20/target/classes/public/create_collection.js
@@ -1,4 +1,4 @@
-import { MongoClient } from 'mongodb';
+var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 
 MongoClient.connect(url, function(err, db) {
--- se-team20/target/classes/public/create_db.js
+++ se-team20/target/classes/public/create_db.js
@@ -1,4 +1,4 @@
-import { MongoClient } from 'mongodb';
+var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/mydb";
 
 MongoClient.connect(url, function(err, db) {
