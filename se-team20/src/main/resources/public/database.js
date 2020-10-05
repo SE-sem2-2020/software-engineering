4e251d24a621614ab4389e00b47c446e37c768f3
CrookedDesk <50561427+CrookedDesk@users.noreply.github.com>
2020-10-06 12:59:46 +1300

    testing sql db con

---
+++ se-team20/src/main/resources/public/database.js
@@ -0,0 +1,12 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "ec2-23-20-168-40.compute-1.amazonaws.com",
+    user: "fgltmkgurkvrcy",
+    password: "db66c2b072423e5956ec608256cd4a64a56ac5d9c796065f5bcc08eaa90f38e4"
+  });
+  
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
