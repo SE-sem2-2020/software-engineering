4e9892e0197f95014f8366782f2ee087e8fa6869
CrookedDesk <50561427+CrookedDesk@users.noreply.github.com>
2020-10-06 12:44:05 +1300

    testing db con

--- se-team20/src/main/resources/public/test_database.js
+++ se-team20/src/main/resources/public/test_database.js
@@ -7,7 +7,7 @@ function connect(dataSource)
 		xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 		xhr.onreadystatechange = function()
 		{
-			//alert(xhr.readyState);
+			alert(xhr.readyState);
 			if (xhr.readyState == 4 && xhr.status == 200)
 			{
 				obj.innerHTML = xhr.responseText;
