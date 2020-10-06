41b09f4738821858bfb0b1a4c70dc382183a05df
CrookedDesk <50561427+CrookedDesk@users.noreply.github.com>
2020-10-06 13:06:37 +1300

    testing sql db con

--- se-team20/src/main/resources/public/database.js
+++ se-team20/src/main/resources/public/database.js
@@ -1,6 +1,6 @@
-var mysql = require('mysql');
+import { createConnection } from 'mysql';
 
-var con = mysql.createConnection({
+var con = createConnection({
     host: "ec2-23-20-168-40.compute-1.amazonaws.com",
     user: "fgltmkgurkvrcy",
     password: "db66c2b072423e5956ec608256cd4a64a56ac5d9c796065f5bcc08eaa90f38e4"
