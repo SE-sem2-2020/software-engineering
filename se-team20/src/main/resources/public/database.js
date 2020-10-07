2891a55535cba8c156691a5cc3e964279276f977
CrookedDesk <50561427+CrookedDesk@users.noreply.github.com>
2020-10-08 11:06:16 +1300

    testing mongodb conn

--- se-team20/src/main/resources/public/database.js
+++ se-team20/src/main/resources/public/database.js
@@ -1,1 +1,8 @@
-var mydb = connect('cluster0-shard-00-02.hfesc.mongodb.net[:27017]/seerdatabase');
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://cluster0-shard-00-02.hfesc.mongodb.net:27017/seerdatabase";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  console.log("Database created!");
+  db.close();
+});
